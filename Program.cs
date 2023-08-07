587dda443d101ae135640f9d35042ce1b9db19be
VictorCstr <victorcastro.dev@hotmail.com>
2023-08-07 12:20:57 -0300

    feat: adicionado Classe de Posicao das pecas

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using Projeto_Xadrez.Entities.Tabuleiro;
+
+namespace Projeto_Xadrez // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3, 4);
+            System.Console.WriteLine(p);
+
+        }
+
+    }
+}
